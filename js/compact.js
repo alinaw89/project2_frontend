2143018a65ef407b544c1eb3bb56a00c63d122cf
alinaw89 <ali.naw89@gmail.com>
2015-06-07 15:56:05 -0400

    added edit and delete buttons

--- js/compact.js
+++ js/compact.js
@@ -94,7 +94,7 @@ $(document).ready(function(){
       headers: { Authorization: 'Token token=' + simpleStorage.get('token') }
     }).done(function(cosmetic_product_data){
       console.log(cosmetic_product_data);
-      var html = "<dl> <dt>Name</dt><dd>" + cosmetic_product_data.name + "<dt>Brand</dt><dd>" + cosmetic_product_data.brand + "<dt>Color</dt><dd>" + cosmetic_product_data.color + "<dt>Price</dt><dd>" + cosmetic_product_data.price + "<dt>Purchase Date</dt><dd>" + cosmetic_product_data.purchase_date + "<dt>Category</dt><dd>" + cosmetic_product_data.category_name + "<dt>Image</dt><dd>" + '<img src="' + cosmetic_product_data.photo + '"/>';
+      var html = "<dl> <dt>Name</dt><dd>" + cosmetic_product_data.name + "<dt>Brand</dt><dd>" + cosmetic_product_data.brand + "<dt>Color</dt><dd>" + cosmetic_product_data.color + "<dt>Price</dt><dd>" + cosmetic_product_data.price + "<dt>Purchase Date</dt><dd>" + cosmetic_product_data.purchase_date + "<dt>Category</dt><dd>" + cosmetic_product_data.category_name + "<dt>Image</dt><dd>" + '<img src="' + cosmetic_product_data.photo + '"/>' + '<button>Edit</button>' + '<button>Delete</button>' ;
       $("#cosmetic_product").html("");
       $("#cosmetic_product").append(html);
     }).fail(function(){
